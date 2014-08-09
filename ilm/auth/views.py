06a083454a6fbd8601b85ec59203188e1017e621
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-09 21:38:37 +0400

    login view added

--- ilm/auth/views.py
+++ ilm/auth/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import FormView
+from django.contrib.auth import REDIRECT_FIELD_NAME, login, logout
 
-# Create your views here.
+class LoginView(FormView):
+    """
+    View to login the user.
+    """
+    def get_success_url(self):
+        """
+        Return success url after login.
+        """
+        next_url = self.request.GET(REDIRECT_FIELD_NAME, None)
+        if not next_url:
+            pass
+        return next_url
+
+    def get(self, request, *args, **kwargs):
+        """
+        Returns to success url if authenticated.
+        """
+        if request.user.is_authenticated():
+            redirect(self.get_success_url())
+        return super(LoginView, self).get(request, *args, *kwargs)
+
+    def form_valid(self, form):
+        """
+        Override form_valid to login.
+        """
+        login(self.request, form.get_user())
+        super(LoginView, self).form_valid(form)
