7d9bce920e4789fb8251ea56ffa110b683b563f0
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-10 21:15:21 +0400

    adding urls

--- ilm/ilm/urls.py
+++ ilm/ilm/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ilm.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^auth/', include('auth.urls')),
 )
