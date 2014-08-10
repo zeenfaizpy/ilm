e53f79ff1d51de9952b3b0a66ce8704e2db19a64
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-10 21:14:09 +0400

    fixing form syntax issues

--- ilm/auth/forms.py
+++ ilm/auth/forms.py
@@ -6,7 +6,8 @@ class LoginForm(AuthenticationForm):
     """
     Overriding default authentication form for crispy.
     """
-    def __init__(self, *args, *kwargs):
-        super(LoginForm, self).__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
+        self.helper.form_class = "form-horizontal"
         self.helper.form_tag = False
