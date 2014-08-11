41211fa67bbefbf47c2855a9444f95054dfd61f1
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-11 22:52:27 +0400

    added helpers app and timestamped model to reuse common fields

---
+++ ilm/helpers/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TimestampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
