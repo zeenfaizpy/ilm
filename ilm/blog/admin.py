3816eff7659d14eb071a450e6d8373f16a318106
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-13 22:57:58 +0400

    tag, category, post added in admin

--- ilm/blog/admin.py
+++ ilm/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Tag, Category
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Category)
