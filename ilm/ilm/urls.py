06da50bc1319330b570b503c4e9ddd29d0bbb6ae
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-17 21:23:36 +0400

    markdown urls added

--- ilm/ilm/urls.py
+++ ilm/ilm/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^auth/', include('auth.urls')),
     url(r'^blog/', include('blog.urls')),
+    url(r'^markdown/', include('django_markdown.urls')),
 )
