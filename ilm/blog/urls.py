85d87f5dbe22011d552044798335db0b67b57bfb
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-18 22:43:02 +0400

    urls for year archive view added

--- ilm/blog/urls.py
+++ ilm/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from .views import ListPostsView, DetailPostView
+from .views import ListPostsView, DetailPostView, PostYearArchiveView
 
 
 urlpatterns = patterns('',
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
                 url(r'^posts/(?P<slug>[-_\w]+)/detail/$',
                     DetailPostView.as_view(),
                     name='detail_post'),
+                url(r'^posts/(?P<year>[0-9]{4})/$',
+                    PostYearArchiveView.as_view(),
+                    name="post_year_archive"),
                 )
