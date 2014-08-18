d77081b0dbc44bbff0307633cc0a909b145e4dd0
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-18 22:42:23 +0400

    enabling mardown editor in admin

--- ilm/blog/admin.py
+++ ilm/blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Post, Tag, Category
 
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
+
+from django_markdown.admin import MarkdownModelAdmin
+admin.site.register(Post, MarkdownModelAdmin)
