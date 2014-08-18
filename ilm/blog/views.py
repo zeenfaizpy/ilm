873ecebc02d82823f8bc0cb73a16809c6f14e9f6
Mohammad Faizal <zeenfaiz@gmail.com>
2014-08-18 22:42:41 +0400

    year archive view implemented

--- ilm/blog/views.py
+++ ilm/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, YearArchiveView
 
 from .models import Post, Tag, Category
 
@@ -17,3 +17,12 @@ class DetailPostView(DetailView):
     """
     model = Post
     template_name = "detail_post.html"
+
+class PostYearArchiveView(YearArchiveView):
+    """
+    """
+    model = Post
+    date_field = "created"
+    template_name = "post_year_archive.html"
+    make_object_list = True
+    allow_future = True
